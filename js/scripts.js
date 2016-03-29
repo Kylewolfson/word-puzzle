f1fb6a5e773a8aa5899d5166c7ebf8666076a5de
Elissa Sosland and Kyle Wolfson <kyle@>
2016-03-29 13:08:10 -0700

    added fix to stop append spam

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var phrase = $("#phrase").val();
     var originalString = phrase;
     var puzzle = phrase.split("");
+    $("#wordPuzzle").text("");
 
     for (var i=0; i < puzzle.length; i +=1) {
     if (puzzle[i].toUpperCase() === ("A") || puzzle[i].toUpperCase() === ("E") || puzzle[i].toUpperCase() === ("I") || puzzle[i].toUpperCase() === ("O") || puzzle[i].toUpperCase() === ("U") || puzzle[i].toUpperCase() === ("Y")) {
