88f9d5ddd0e067a62b5061735e78db489a81f94a
Elissa Sosland and Kyle Wolfson <kyle@>
2016-03-29 11:50:37 -0700

    have a functional word guessing game with the exception of spitting out the puzzle multiple times due to using append

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var phrase = $("#phrase").val();
+    var originalString = phrase;
+    var puzzle = phrase.split("");
+
+    for (var i=0; i < puzzle.length; i +=1) {
+    if (puzzle[i].toUpperCase() === ("A") || puzzle[i].toUpperCase() === ("E") || puzzle[i].toUpperCase() === ("I") || puzzle[i].toUpperCase() === ("O") || puzzle[i].toUpperCase() === ("U") || puzzle[i].toUpperCase() === ("Y")) {
+    puzzle[i] = ("-");
+    }
+    $("#wordPuzzle").append(puzzle[i]);
+    }
+    $("#inputForm").hide();
+
+    if ($("#puzzle").val().toUpperCase() === originalString.toUpperCase()) {
+      alert("you win!");
+    }
+  });
+});
